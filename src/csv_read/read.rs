c39c58df9062fe63c72b72b7a4f38d276d969e9f
Josh <tarv360@gmail.com>
2018-08-27 19:55:20 +0930

    Functionality for getting wind speed data

--- src/csv_read/read.rs
+++ src/csv_read/read.rs
@@ -10,9 +10,9 @@ use std::num::ParseIntError;
 
 #[derive(Debug, Clone, Copy, Deserialize, Serialize)]
 pub struct Date {
-    year: Option<u32>,
-    month: Option<u32>,
-    day: Option<u32>,
+    pub year: Option<u32>,
+    pub month: Option<u32>,
+    pub day: Option<u32>,
 }
 
 impl FromStr for Date {
@@ -97,6 +97,52 @@ impl FromStringRecord for TempStation {
     }
 }
 
+#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
+pub struct WindStation {
+    pub date: Option<Date>,
+    pub longitude: Option<f32>,
+    pub latitude: Option<f32>,
+    pub avg_wind: Option<f32>,
+}
+
+impl FromStringRecord for WindStation {
+    fn field_count() -> usize {
+        4
+    }
+
+    fn from_string_records<T: Read>(
+        headers: &HeaderContainer,
+        records: &mut StringRecordsIter<T>,
+    ) -> Result<Vec<Self>, Box<Error>> {
+        if Self::correct_num_headers(headers) {
+            let long_col = headers.column_with_name("LONGITUDE")?;
+            let lat_col = headers.column_with_name("LATITUDE")?;
+            let date_col = headers.column_with_name("DATE")?;
+            let avg_wind_col = headers.column_with_name("AWND")?;
+            
+
+            let mut values = Vec::new();
+            for record in records {
+                let record = record?;
+                let mut long = record[long_col].parse().ok();
+                let mut lat = record[lat_col].parse().ok();
+                let mut date = record[date_col].parse().ok();
+                let mut avg_wind = record[avg_wind_col].parse().ok();
+                if avg_wind != None {
+                    values.push(Self {
+                        date,
+                        longitude: long,
+                        latitude: lat,
+                        avg_wind
+                    });
+                }
+            }
+            return Ok(values);
+        }
+        Err(Box::new(IncorrectColumnsErr::new(headers.clone())))
+    }
+}
+
 pub fn get_temp_stations<P: AsRef<Path>>(path: P) -> Result<Vec<TempStation>, Box<Error>> {
     let mut reader = Reader::from_path(path)?;
     let headers;
@@ -109,5 +155,20 @@ pub fn get_temp_stations<P: AsRef<Path>>(path: P) -> Result<Vec<TempStation>, Bo
         )?;
     }
     let values = TempStation::from_string_records(&headers, &mut reader.records())?;
+    Ok(values)
+}
+
+pub fn get_wind_stations<P: AsRef<Path>>(path: P) -> Result<Vec<WindStation>, Box<Error>> {
+    let mut reader = Reader::from_path(path)?;
+    let headers;
+    {
+        let header_record = reader.headers()?;
+        headers = HeaderContainer::from_record(
+            header_record,
+            &["DATE", "LONGITUDE", "LATITUDE", "AWND"],
+            &mut vec![],
+        )?;
+    }
+    let values = WindStation::from_string_records(&headers, &mut reader.records())?;
     Ok(values)
 }
